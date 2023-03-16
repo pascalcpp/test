9789d7d5f8e9dac21350323300538c5e094d9b90
pascalcpp <1287971058@qq.com>
2023-03-16 23:49:51 +0800

    Create 1012.cpp

---
+++ 1012.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<string.h>
+#define MAX 10000
+typedef struct Node {
+	int data;
+    struct Node* next;
+}Node;
+int main() {
+	int m, n, val;
+    Node* hashmap[MAX];
+    memset(hashmap, 0, sizeof(hashmap));
+    scanf("%d", &m);
+	scanf("%d", &n);
+    for(int i = 0; i < m; i++) {
+    	int temp;
+        scanf("%d", &temp);
+        Node* node = new Node;
+        node->data = temp;
+        node->next = nullptr;
+        int hash = temp%m;
+        if(hashmap[hash] != nullptr) {
+        	Node* cur = hashmap[hash];
+            while(cur->next != nullptr) {
+            	cur = cur->next;
+            }
+            cur->next = node;
+        } else {
+        	hashmap[hash] = node;
+        }
+    }
+    scanf("%d", &val);
+    int hash = val%m;
+    Node* cur = hashmap[hash];
+    int c = 0;
+    while(cur != nullptr && cur->data != val) {
+        c++;
+        cur = cur->next;
+    }
+    if(cur == nullptr) {
+    	printf("-1");
+    } else {
+    	printf("%d,%d", hash, c+1);
+    }
+
+	return 0;
+}
