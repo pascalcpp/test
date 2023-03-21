d7e9a73a042d890ae0f7759ac7bad25904ba0ae3
pascalcpp <1287971058@qq.com>
2023-03-21 17:31:29 +0800

    Update 1075.cpp

--- 1075.cpp
+++ 1075.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<string.h>
 using namespace std;
 
 typedef struct
@@ -61,38 +60,39 @@ void create(Graph &G,int n ,int e)
     G.n=n;
     G.e=e;
 }
-int uset[128];
-int visit[128];
-EDGE edges[256];
+int closest[500];
+int lowcost[500];
 void Prim(Graph &G,int k)
 {
-    memset(visit, 0x00, sizeof(visit));
-    int nu = 0, ne = 0;
-    uset[nu++] = k;
-    visit[k] = 1;
-    for(int i = 0; i < G.n-1; i++) {
-        mincost mc;
-        mc.cost = 100;
-        int end;
-        for(int j = 0; j < nu; j++) {
-        	for(int p = 0; p < G.n; p++) {
-            	if((G.edge[uset[j]][p] > 0 && G.edge[uset[j]][p] < 100) &&
-                  !visit[p] && G.edge[uset[j]][p] < mc.cost) {
-               		mc.cost = G.edge[uset[j]][p];
-                    mc.index = uset[j];
-                    end = p;
-                }
+
+    for(int i = 0; i < G.n; i++) {
+    	closest[i] = k;
+    }
+	for(int i = 0; i < G.n; i++) {
+    	// k -> i
+        lowcost[i] = G.edge[k][i];
+    }
+    for(int i = 1; i < G.n; i++) {
+    	int min = 100;
+        int v;
+        for(int j = 0; j < G.n; j++) {
+        	if(lowcost[j] > 0 && lowcost[j] < min) {
+            	min = lowcost[j];
+                v = j;
             }
         }
-        visit[end] = 1;
-        uset[nu++] = end;
-       	edges[ne].x = mc.index, edges[ne].y = end;
-        edges[ne].weight = mc.cost;
-        ne++;
+        printf("(%c,%c)", G.data[closest[v]], G.data[v]);
+      	lowcost[v] = 0;
+        for(int j = 0; j < G.n; j++) {
+            // V-U vertexs
+      		if(lowcost[j] > 0 && G.edge[v][j] > 0 && G.edge[v][j] < lowcost[j]) {
+                lowcost[j] = G.edge[v][j];
+            	closest[j] = v;
+            }
+        }
     }
-    for(int i = 0; i < ne; i++) {
-    	printf("(%c,%c)", G.data[edges[i].x], G.data[edges[i].y]);
-    }
+
+
     //完成Prim算法
 
 }
